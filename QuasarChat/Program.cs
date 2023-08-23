d279ef01c6dc3457c6824e1baa025d5faeec2505
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-22 23:22:30 -0300

    Adicionar arquivos de projeto.

---
+++ QuasarChat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+
+namespace QuasarChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(3000);
+            server.Start();
+
+            while(true)
+            {
+                Console.WriteLine("Observando Conexões...");
+                Socket connection = server.AcceptSocket();
+            }
+        }
+    }
+}
---
+++ QuasarClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QuasarClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
