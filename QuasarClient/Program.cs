904f4e1018393fd106151a7c63c8fb5b51c72fe4
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-08-22 23:40:02 -0300

    feat: create client

--- QuasarClient/Program.cs
+++ QuasarClient/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace QuasarClient
+﻿using System.Net.Sockets;
+
+namespace QuasarClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 3000);
+
+            NetworkStream outStream = client.GetStream();
+
+            BinaryWriter writer = new BinaryWriter(outStream);
+            BinaryReader reader = new BinaryReader(outStream);
+
+            client.Close();
         }
     }
 }
